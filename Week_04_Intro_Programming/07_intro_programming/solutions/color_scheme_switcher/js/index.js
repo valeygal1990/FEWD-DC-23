28c1a68438d22b3af976781236e01b24beb19426
valeygal1990 <valentinesakwa@Valentines-MacBook-Air.local>
2016-04-12 18:47:26 -0400

    Joni's changes

--- Week_04_Intro_Programming/07_intro_programming/solutions/color_scheme_switcher/js/index.js
+++ Week_04_Intro_Programming/07_intro_programming/solutions/color_scheme_switcher/js/index.js
@@ -1,5 +1,6 @@
 document.getElementById('grayButton').onclick = switchGray;
 document.getElementById('whiteButton').onclick = switchWhite;
+document.getElementById('redButton').onclick = switchRed;
 
 function switchGray() {
     document.body.style.backgroundColor = 'gray';
@@ -10,3 +11,9 @@ function switchWhite() {
     document.body.style.backgroundColor = 'white';
     document.body.style.color = 'black';
 }
+
+function switchRed() {
+	document.body.style.backgroundColor = 'red';
+	document.body.style.color = 'white'
+}
+
--- Week_04_Intro_Programming/08_jQuery/starter_code/traffic_light/js/index.js
+++ Week_04_Intro_Programming/08_jQuery/starter_code/traffic_light/js/index.js
@@ -1,1 +1,25 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+$(document).ready(function())
+
+$('#stopButton').click(colorStopLight);
+
+function colorStopLight(){
+	$('#stopLight').css('backgroundColor','red');
+	$('#slowLight, #goLight').css('backgroundColor','black');
+}
+
+$('#slowButton').click(colorSlowLight);
+
+function colorSlowLight(){
+	$('#slowLight').css('backgroundColor','yellow');
+	$('#stopLight, #goLight').css('backgroundColor','black');
+}
+
+$('#goButton').click(colorGoLight);
+
+function colorGoLight(){
+	$('#goLight').css('backgroundColor','green');
+	$('#stopLight, slowLight').css('backgroundColor','black');
+}
+
+});
